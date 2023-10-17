985762f04f79b0293a2254c52a572fe27bc1c385
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-17 20:01:23 +0300

    initial commit

---
+++ src/app/AppModule.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+  
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrderHistoryComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
