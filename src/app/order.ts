fb0549bc14d3f2cc67fef0c7ea28772dac1c828a
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-17 22:06:16 +0300

    added constructors of order and order item models

--- src/app/order.ts
+++ src/app/order.ts
@@ -1,2 +1,12 @@
+import { OrderItem } from "./order-item";
+
 export class Order {
+  constructor(
+    public id: number,
+    public guestEmail: string,
+    public couponID: number,
+    public amount: number,
+    public createdAt: Date,
+    public orderItems: OrderItem[]
+  ) {}
 }
