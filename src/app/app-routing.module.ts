f7d8007641739ddfc8844b9947f063024cfffe57
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-17 22:09:51 +0300

    added order history component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'order', component: OrderHistoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Order } from '../order';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-order-history',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-history.component.scss']
 })
 export class OrderHistoryComponent {
+  orders : Order[] = [];
+  guestEmail: string = '';
+  fromDate: string = '';
+  toDate: string = '';
 
+  constructor(private service : OrderService){}
+
+  ngOnInit() : void {
+    this.getAllOrders();
+  }
+
+  getAllOrders(){
+    this.service.getAllOrders().subscribe({
+      next: (orders) => { this.orders = orders; }, 
+      error: (error) => {
+        this.orders = [];
+      }
+    })
+  }
+
+  filterOrders(){
+    if(this.guestEmail){
+      this.service.getOrdersByGuestEmail(this.guestEmail).subscribe({
+        next: (orders) => { this.orders = orders; },
+        error: (error) => {
+          this.orders = [];
+        }
+      })
+    } else if(this.fromDate || this.toDate){
+      this.service.getOrdersByDateRange(this.fromDate, this.toDate).subscribe({
+        next: (orders) => { this.orders = orders; },
+        error: (error) => {
+          this.orders = [];
+        }
+      })
+    } else {
+      this.getAllOrders();
+    }
+  }
 }
