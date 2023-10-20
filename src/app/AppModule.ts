8f21a8fab6f277bc3d3db54e231d12af37f1e8b9
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-20 16:51:53 +0300

    imported required modules and handled routing paths

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -4,19 +4,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgxsModule } from '@ngxs/store';
+import { CartState } from './services/CartService/cart.state';
+
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ViewStoresComponent } from './components/view-stores/view-stores.component';
+import { StoreHomeComponent } from './components/store-home/store-home.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
   
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewStoresComponent,
+    StoreHomeComponent,
+    ShoppingCartComponent
   ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule,
+
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    
+    NgxsModule.forRoot([
+      CartState,
+    ]),
   ],
   
   providers: [],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreHomeComponent } from './components/store-home/store-home.component';
+import { ViewStoresComponent } from './components/view-stores/view-stores.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
+  {path: 'store/shopping-cart', component: ShoppingCartComponent},
+  {path: 'store/:storeId', component: StoreHomeComponent},
+  {path: 'store', component: ViewStoresComponent}
 ];
 
 @NgModule({
