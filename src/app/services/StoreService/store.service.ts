f0739f6a23d341310d3d0a10e27b518538e748cd
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-25 16:08:12 +0300

    updated store service layer

--- src/app/services/StoreService/store.service.ts
+++ src/app/services/StoreService/store.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
+import { OrderRequestItem } from 'src/app/models/OrderRequestItem/order-request-item';
 import { Product } from 'src/app/models/Product/product';
+import { ProductConsumption } from 'src/app/models/ProductConsumption/product-consumption';
 import { Store } from 'src/app/models/Store/store';
 
 @Injectable({
@@ -10,7 +12,7 @@ import { Store } from 'src/app/models/Store/store';
 
 export class StoreService {
 
-  private baseUrl : string = 'http://localhost:51638/store'
+  private baseUrl : string = 'http://localhost:56741/store'
 
   constructor(private http:HttpClient) { }
 
@@ -33,4 +35,44 @@ export class StoreService {
       })
     );
   }  
+
+  getStoreInfo(storeId : number) : Observable<Store> {   
+    return this.http.get<Store>(`${this.baseUrl}/get?storeId=${storeId}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
+  }
+  
+  addStore(store: Store): Observable<Store | HttpErrorResponse> {
+    return this.http.post<Store>(`${this.baseUrl}`, store).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  addStock(OrderRequestItem: OrderRequestItem): Observable<any | HttpErrorResponse> {
+    return this.http.post<any>(`${this.baseUrl}/addStock`, OrderRequestItem).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getAllProductConsumptions(): Observable<ProductConsumption[]> {
+    return this.http.get<ProductConsumption[]>(`${this.baseUrl}/getAllProductConsumptions`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
+  }
+  
+  getProductConsumptionsByStoreId(storeId: number): Observable<ProductConsumption[]> {
+    return this.http.get<ProductConsumption[]>(`${this.baseUrl}/getProductConsumptionsByStoreId?storeId=${storeId}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
+  }
 }
