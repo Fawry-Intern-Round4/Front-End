7b1f38e5ccb2f99cbfd6455b82964e885e447a03
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-25 16:09:43 +0300

    updated models

---
+++ src/app/models/ProductConsumption/product-consumption.spec.ts
@@ -0,0 +1,7 @@
+import { ProductConsumption } from './product-consumption';
+
+describe('ProductConsumption', () => {
+  it('should create an instance', () => {
+    expect(new ProductConsumption()).toBeTruthy();
+  });
+});
