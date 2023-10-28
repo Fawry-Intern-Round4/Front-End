a04cc676cd887a9443401156f3956a5786684797
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-10-28 17:25:11 +0200

    project rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ConsumptionUI
+# FrontEnd
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'manage-products-angular';
+  title = 'front-end';
 }
